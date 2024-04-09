9d74d883b1b0886eec7acf020700ad054f391c24
Alex Zh <alzh147@gmail.com>
2024-04-09 21:12:04 +0500

    prikruchivanie Allure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 [![Java CI with Gradle](https://github.com/alexzh14/patterns_task1/actions/workflows/gradle.yml/badge.svg)](https://github.com/alexzh14/patterns_task1/actions/workflows/gradle.yml)
+
+![](C:\Users\User\Downloads\Allure Report 2024-04-09 21-07-04.png)
+![](C:\Users\User\Downloads\Allure Report 2024-04-09 21-10-58.png)
--- src/test/java/ru/netology/delivery/Tests.java
+++ src/test/java/ru/netology/delivery/Tests.java
@@ -1,6 +1,10 @@
 package ru.netology.delivery;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -14,6 +18,16 @@ import static com.codeborne.selenide.Selenide.open;
 
 
 public class Tests {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll () {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setup() {
